026fcdb3978b3c87aecd400a9c20789c7d415d85
souhad <souhadmoussa86@gmail.com>
2024-02-22 21:27:49 +0200

    fetch artisan page

--- homedy/src/components/BestSeller.js
+++ homedy/src/components/BestSeller.js
@@ -1,46 +1,48 @@
-import { products } from "../data/Data";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Heading from "../common/Heading";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Heading from "../common/Heading";
 
 const Category = () => {
-  var settings = {
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 640,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
+  const [allArtists, setAllArtists] = useState([]);
+  const [artists, setArtists] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-  const [menuItem, setMenuItem] = useState(products);
-  const filterItems = (name) => {
-    const newItems = products.filter((item) => item.name === name);
-    setMenuItem(newItems);
+  useEffect(() => {
+    fetchArtists();
+    fetchCategories();
+  }, []);
 
-    // for all data show
-    if (name === "all") {
-      setMenuItem(products);
-      return;
+  const fetchArtists = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/artist/artists");
+      const data = await response.json();
+      setAllArtists(data);
+      setArtists(data); // Initially, set all artists to be shown
+    } catch (error) {
+      console.error("Error fetching artists:", error);
     }
   };
+
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/category/read/all");
+      const data = await response.json();
+      setCategories(data);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
+  const filterItems = async (categoryId) => {
+    try {
+      const response = await fetch(`http://localhost:5000/artist/${categoryId}`);
+      const data = await response.json();
+      setArtists(data);
+    } catch (error) {
+      console.error("Error filtering artists:", error);
+    }
+  };
+  
 
   return (
     <div className="bg-white mt-16 text-center">
@@ -49,104 +51,36 @@ const Category = () => {
         description="LOREM IPSUM IS SIMPLY DUMMY TEXT OF THE PRINTING AND TYPESETTING INDUSTRY"
       />
       <div className="flex justify-center">
-        <button
-          type="button"
-          className="bg-gray-200 pt-2 pb-2 pl-4 pr-4 mr-2 text-black font-semibold hover:bg-black hover:text-white transition-all duration-300"
-          onClick={() => filterItems("all")}
-        >
-          All
-        </button>
-        <button
-          type="button"
-          className="bg-gray-200 pt-2 pb-2 pl-4 pr-4 mr-2 text-black font-semibold hover:bg-black hover:text-white transition-all duration-300"
-          onClick={() => filterItems("DECOR")}
-        >
-          DECOR
-        </button>
-        <button
-          type="button"
-          className="bg-gray-200 pt-2 pb-2 pl-4 pr-4 mr-2 text-black font-semibold hover:bg-black hover:text-white transition-all duration-300"
-          onClick={() => filterItems("KITCHEN")}
-        >
-          KITCHEN
-        </button>
-        <button
-          type="button"
-          className="bg-gray-200 pt-2 pb-2 pl-4 pr-4 mr-2 text-black font-semibold hover:bg-black hover:text-white transition-all duration-300"
-          onClick={() => filterItems("LIVING ROOM")}
-        >
-          LIVING ROOM
-        </button>
-        <button
-          type="button"
-          className="bg-gray-200 pt-2 pb-2 pl-4 pr-4 mr-2 text-black font-semibold hover:bg-black hover:text-white transition-all duration-300"
-          onClick={() => filterItems("LIVING ROOM")}
-        >
-          Candles
-        </button>
-        <button
-          type="button"
-          className="bg-gray-200 pt-2 pb-2 pl-4 pr-4 mr-2 text-black font-semibold hover:bg-black hover:text-white transition-all duration-300"
-          onClick={() => filterItems("LIVING ROOM")}
-        >
-          soap
-        </button>
-        <button
-          type="button"
-          className="bg-gray-200 pt-2 pb-2 pl-4 pr-4 mr-2 text-black font-semibold hover:bg-black hover:text-white transition-all duration-300"
-          onClick={() => filterItems("SOFA")}
-        >
-          SOFA
-        </button>
-      </div>
-      <div
-        className=" m-10 overflow-hidden text-center relative grid grid-cols-4 flex-wrap"
-        {...settings}
-      >
-        {menuItem.map((item, index) => (
-          <div key={index} className="relative">
-            <div className="m-4 relative group">
-            <Link to={`/product/${item.id}`}> 
-
-
-              <img
-                src={item.img}
-                title={item.name}
-                alt={item.name}
-                className="mx-auto h-full w-full hover:shadow-md transition-all duration-300 bg-cover"
-              />
-              </Link>
-              <div className="icons absolute top-0 right-0 transform translate-x-full opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition-all duration-300">
-                <div className="flex flex-wrap flex-col  p-2 mr-1 mt-1">
-                  <FontAwesomeIcon
-                    className="p-2 mb-1 bg-white hover:bg-red-500 hover:text-white"
-                    title="add to cart"
-                    icon="shopping-cart"
-                  />
-                  <FontAwesomeIcon
-                    className="p-2 mb-1 bg-white hover:bg-red-500 hover:text-white"
-                    title="add to cart"
-                    icon="heart"
-                  />
-                  <FontAwesomeIcon
-                    className="p-2 mb-1 bg-white hover:bg-red-500 hover:text-white"
-                    title="add to cart"
-                    icon="balance-scale"
-                  />
-                  <FontAwesomeIcon
-                    className="p-2 mb-1 bg-white hover:bg-red-500 hover:text-white"
-                    title="add to cart"
-                    icon="eye"
-                  />
-                </div>
-              </div>
-              <div className="mt-4">
-                <div className="font-semibold uppercase">{item.name}</div>
-                <div className="font-semibold uppercase">{item.price}</div>
-              </div>
-            </div>
-          </div>
+        {/* Render category buttons */}
+        {categories.map((category) => (
+          <button
+            key={category._id}
+            type="button"
+            className="bg-gray-200 pt-2 pb-2 pl-4 pr-4 mr-2 text-black font-semibold hover:bg-black hover:text-white transition-all duration-300"
+            onClick={() => filterItems(category._id)}
+          >
+            {category.title}
+          </button>
         ))}
+      </div>
+      <div className="m-10 overflow-hidden text-center relative grid grid-cols-4 flex-wrap">
+      {artists.map((artist, index) => (
+  <div key={index} className="relative">
+    <div className="m-4 relative group">
+      <Link to={`/product/${artist.userId._id}`}>
+        <img
+          src={`${process.env.REACT_APP_BACKEND}/${artist.userImage || artist.userId.image}`}
+          alt={artist.BrandName}
+          className="mx-auto h-full w-full hover:shadow-md transition-all duration-300 bg-cover"
+        />
+      </Link>
+      <div className="mt-4">
+        <div className="font-semibold uppercase">{artist.BrandName}</div>
+      </div>
+    </div>
+  </div>
+))}
+
       </div>
     </div>
   );
